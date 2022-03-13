cccbd354dc8fecf94306fc45b5ea35d045b46bc1
Ario Setiawan <consciousness.dev@gmail.com>
2022-03-13 15:15:37 +0700

    On Progress Testing -  Fail Test

--- src/actions.js
+++ src/actions.js
@@ -1,16 +1,28 @@
-import { apiCall } from './api/api'
+import { apiCall } from './api/api';
 import {
   CHANGE_SEARCHFIELD,
   REQUEST_ROBOTS_PENDING,
   REQUEST_ROBOTS_SUCCESS,
-  REQUEST_ROBOTS_FAILED
- } from './constants'
+  REQUEST_ROBOTS_FAILED,
+} from './constants';
 
-export const setSearchField = (text) => ({ type: CHANGE_SEARCHFIELD, payload: text })
+export const setSearchField = (text) => ({
+  type: CHANGE_SEARCHFIELD,
+  payload: text,
+});
 
-export const requestRobots = () => (dispatch) => {
-  dispatch({ type: REQUEST_ROBOTS_PENDING })
-  apiCall('https://jsonplaceholder.typicode.com/users')
-    .then(data => dispatch({ type: REQUEST_ROBOTS_SUCCESS, payload: data }))
-    .catch(error => dispatch({ type: REQUEST_ROBOTS_FAILED, payload: error }))
-}
+export const requestRobots = (
+  apiUrl = 'https://jsonplaceholder.typicode.com/users'
+) => async (dispatch) => {
+  dispatch({ type: REQUEST_ROBOTS_PENDING });
+  // await apiCall(apiUrl)
+  //   .then((data) => dispatch({ type: REQUEST_ROBOTS_SUCCESS, payload: data }))
+  //   .catch((error) =>
+  //     dispatch({ type: REQUEST_ROBOTS_FAILED, payload: error })
+  //   );
+  const data = await apiCall(apiUrl);
+  if (data) {
+    return dispatch({ type: REQUEST_ROBOTS_SUCCESS, payload: data });
+  }
+  return dispatch({ type: REQUEST_ROBOTS_FAILED, payload: data });
+};
--- src/actions.test.js
+++ src/actions.test.js
@@ -22,16 +22,39 @@ it('should create an action to search robots', () => {
 });
 
 it('handles requesting robots API', () => {
-    const store = mockStore(); // mocking store
+  const store = mockStore(); // mocking store
 
-    store.dispatch(actions.requestRobots());
-    // get dispatched request robots which is sent from App.js
-    
-    const action = store.getActions();
-    // action = [{ type: REQUEST_ROBOTS_PENDING, }]
+  store.dispatch(actions.requestRobots());
+  // get dispatched request robots which is sent from App.js
 
-    const expectedAction = {
-        type: REQUEST_ROBOTS_PENDING,
-    };
+  const action = store.getActions();
+  // action = [{ type: REQUEST_ROBOTS_PENDING, }]
+
+  const expectedAction = {
+    type: REQUEST_ROBOTS_PENDING,
+  };
   expect(action[0]).toEqual(expectedAction);
+});
+
+it('handles get result success fetched data', async () => {
+  const store = mockStore();
+  return store.dispatch(actions.requestRobots()).then(() => {
+    const action = store.getActions();
+    const expectedAction = {
+      type: REQUEST_ROBOTS_SUCCESS,
+    };
+    expect(action[1].type).toEqual(expectedAction.type);
+  });
+});
+
+// On Progress
+it('handles get result fail fetched data', () => {
+  // const store = mockStore();
+  // return store.dispatch(actions.requestRobots('/users')).catch(() => {
+  //   const action = store.getActions();
+  //   const expectedAction = {
+  //     type: REQUEST_ROBOTS_FAILED,
+  //   };
+  //   expect(action[1].type).toEqual(expectedAction.type);
+  // });
 });
